6613e3213b98282d53f7e8107ab3c0dbb2b9ab8d
Guille <gguasp@cifpfbmoll.eu>
2020-04-14 20:22:07 +0200

    Provisional index finished

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -12,12 +12,12 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
+                background-image: url("{{ asset('img/bg.jpg') }}");
             }
 
             .full-height {
@@ -46,53 +46,50 @@
 
             .title {
                 font-size: 84px;
+                text-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black;
             }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
 
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            #main{
+                margin-top: 10%;
+                color: white;
+            }
+
+            #logindiv{
+                background-color: none;
+                display: flex;
+                margin: auto;
+                justify-content: space-around;
+            }
+
+            #logindiv a{
+                margin: 15px;
+                background-color: white;
+                color: black;
+                padding: 8px;
+                font-weight: bold;
+                font-size: 1.3em;
+                border-radius: 5px;
+            }
+
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div id="main">
+                    <h1 class="title"><b>The Game</b></h1>
+                    <br>
+                    <div class="container">
+                        <div id="logindiv">
+                            <a href="{{ route('login') }}">Login</a>
+                            <a href="{{ route('register') }}">Register</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
