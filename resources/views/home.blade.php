52fe0507bd34320feab16bb44303fb80f1d8e153
Guille <gguasp@cifpfbmoll.eu>
2020-04-25 20:53:52 +0200

    town view hecha por ahora, falta hacerla responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+<div id="town_name">TOWN NAME</div>
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12 col-lg-12 main-game">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                <div class="resources">
+                    <span id="amount_wood">Wood: xxx</span> 
+                    <span id="amount_stone">Stone: xxx</span> 
                 </div>
+                <div id="woodsman_hut">WOODSMAN</div>
+                <div id="mine">MINE</div>
+                <div id="townhall">TOWNHALL</div>
+                <div id="fisherman">FISHERMAN</div>
+                <div id="potion_shop">POTIONSHOP</div>
+                <div id="farmhouse">FARMHOUSE</div>
+                <div id="merchant">MERCHANT</div>
             </div>
         </div>
     </div>
