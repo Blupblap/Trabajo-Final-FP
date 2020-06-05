78d7845cc7e22ceed688e4e86d813a3c00c67b30
Guille <gguasp@cifpfbmoll.eu>
2020-06-05 20:30:42 +0200

    About done

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div id="about">
+            @lang('custom.aboutContent')
+    </div>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('ranking') }}">{{ __('custom.ranking') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('about') }}">{{ __('custom.about') }}</a>
+                        </li>
                     </ul>
 
                     @if (Route::currentRouteName() !== 'welcome')
